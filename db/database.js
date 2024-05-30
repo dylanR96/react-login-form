acbb503319a78bd9552537d71d323bf6b48dc5f6
dylanR96 <dylan.ravenson@gmail.com>
2024-05-30 18:34:54 +0200

    Added file path for database to include file in db folder

--- db/database.js
+++ db/database.js
@@ -1,5 +1,15 @@
 import DataStore from "nedb";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const database = new DataStore({ filename: "users.db", autoload: true });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const database_directory = `${__dirname}\\users.db`;
+
+const database = new DataStore({
+  filename: database_directory,
+  autoload: true,
+});
 
 export default database;
